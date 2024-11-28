3ae5c52a3683d7aeb7bb5a7d8e180b15fe847b0e
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2024-11-28 11:43:07 +0100

    fix: use develcity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version "3.18.1"
+    id("com.gradle.develocity") version "3.18.2"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.13"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
@@ -21,21 +21,22 @@ plugins {
 fun tag(format: String, vararg properties: String): String =
     format.format(*properties.map { System.getProperty(it) ?: error("Missing property $it") }.toTypedArray())
 
-val ci = !System.getenv("CI").isNullOrBlank()
+val ci = System.getenv("CI").takeUnless { it.isNullOrBlank() }?.equals("true", true) ?: false
 val ciTag = if (ci) "CI" else "Local"
 val osTag = "OS: " + tag("%s (%s) v. %s", "os.name", "os.arch", "os.version")
 val jvmTag = "JVM: " + tag("%s v. %s", "java.vm.name", "java.vm.version")
 val whenTag = "When: ${ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT)}"
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
         tag(ciTag)
         tag(osTag)
         tag(jvmTag)
         tag(whenTag)
-        publishOnFailure()
+        uploadInBackground = !ci
+        publishing.onlyIf { it.buildResult.failures.isNotEmpty() }
         buildScanPublished {
             if (ci) {
                 println("::error title=Gradle scan for $osTag, $jvmTag, $whenTag::$buildScanUri")
